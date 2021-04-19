4d4cc928599be4bb6e0647752e94abd3d4dbf496
rajkumarmariappan <rajkumar.mariappan88@gmail.com>
2021-04-19 22:33:32 +0530

    3rd time same line

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -14,7 +14,7 @@ public class Runner extends BrowserStackConfig {
         home.enterPolicyDetails();
        
         System.out.println("check today");
-        System.out.println("19april");
+        System.out.println("check before merge");
     }
 
 
